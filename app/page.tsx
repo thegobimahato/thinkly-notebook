e112b722162b4a12295edde5d2f57df7fddc5b2c
Gobi Mahato <thegobimahato@gmail.com>
2025-06-23 18:30:36 +0530

    auth setup with kinde auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,37 @@
+import { Button } from "@/components/ui/button";
+import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <section className="background-gradient flex h-screen items-center justify-center">
+      <div className="relative mx-auto w-full max-w-7xl items-center px-5 py-12 md:px-12 lg:px-16">
+        <div className="mx-auto max-w-3xl text-center">
+          <div>
+            <span className="dark:bg-secondary w-auto rounded-full bg-black/70 px-6 py-2.5">
+              <span className="font-medium text-white">
+                Easily organize your notes
+              </span>
+            </span>
+
+            <h1 className="mt-8 text-3xl font-extrabold tracking-tight lg:text-6xl">
+              Create Notes with ease
+            </h1>
+
+            <p className="text-secondary-foreground mx-auto mt-8 max-w-xl text-base italic lg:text-xl">
+              Thinkly makes it simple to jot down thoughts, organize notes, and
+              boost your creativity. All in one place.
+            </p>
+          </div>
+
+          <div className="mx-auto mt-10 flex max-w-sm justify-center">
+            <RegisterLink>
+              <Button size={"lg"} className="w-full">
+                Sign up for free
+              </Button>
+            </RegisterLink>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- components/general/Navbar.tsx
+++ components/general/Navbar.tsx
@@ -2,21 +2,46 @@ import Link from "next/link";
 import React from "react";
 import { ThemeToggle } from "../ui/theme-toggle";
 import { Button } from "../ui/button";
+import {
+  LoginLink,
+  LogoutLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import Image from "next/image";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const { isAuthenticated } = getKindeServerSession();
+
   return (
-    <nav className="bg-background dark:border-accent flex h-[10vh] items-center border-b border-black/20">
+    <nav className="dark:border-accent flex h-[10vh] items-center border-b border-black/20 bg-transparent">
       <div className="container mx-auto flex max-w-7xl items-center justify-between">
         <Link href={"/"}>
-          <h1 className="text-3xl font-bold">Thinkly</h1>
+          <div className="flex items-center justify-center gap-2">
+            <Image src={"./logo.svg"} width={40} height={40} alt="logo" />
+            <h1 className="text-3xl font-bold italic">Thinkly</h1>
+          </div>
         </Link>
 
         <div className="flex items-center gap-x-5">
           <ThemeToggle />
 
           <div className="flex items-center gap-x-5">
-            <Button>Sign In</Button>
-            <Button variant={"secondary"}>Sign Up</Button>
+            {(await isAuthenticated()) ? (
+              <LogoutLink>
+                <Button variant={"destructive"}>Log out</Button>
+              </LogoutLink>
+            ) : (
+              <>
+                <LoginLink>
+                  <Button>Sign In</Button>
+                </LoginLink>
+
+                <RegisterLink>
+                  <Button variant={"secondary"}>Sign Up</Button>
+                </RegisterLink>
+              </>
+            )}
           </div>
         </div>
       </div>
